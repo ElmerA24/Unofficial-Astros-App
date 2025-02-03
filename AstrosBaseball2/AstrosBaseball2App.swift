c3247d981d39aa783de5db2a3397a853f09d1d71
Elmer Arce <158310820+ElmerA24@users.noreply.github.com>
2025-02-03 08:37:12 -0600

    Initial Commit

---
+++ AstrosBaseball2/AstrosBaseball2App.swift
@@ -0,0 +1,17 @@
+//
+//  AstrosBaseball2App.swift
+//  AstrosBaseball2
+//
+//  Created by Elmer Arce on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct AstrosBaseball2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
